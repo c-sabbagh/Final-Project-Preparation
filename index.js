8126fad1ff7fb6613cd977ddbd57e115bdda9e47
Christine <christinesabbagh.tech@gmail.com>
2022-10-18 17:33:26 -0400

    final commit

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-// API 1: "https://jsonplaceholder.typicode.com/users"
-// API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
-const userListEl = document.querySelector('.user-list');
+// // API 1: "https://jsonplaceholder.typicode.com/users"
+// // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListEl = document.querySelector(".user-list")
 
-async function main() {
+async function main(){
     const users = await fetch("https://jsonplaceholder.typicode.com/users")
     const usersData = await users.json()
-    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("")
+    userListEl.innerHTML = usersData.map(user => userHTML(user)).join("")
 }
 
 main();
 
-// how to route to a new page in vanilla JavaScript
-function showUserPosts(id) {
-    window.location.href = `${window.location.origin}/user.html`
-}
-
+function showUserPosts(id){
+    localStorage.setItem("id",id)
+    window.location.href = `${window.location.origin}/user.html` //how to route to a new page in vanilla Javascript? window.loctaion.href = ` `
+} 
+    
 function userHTML(user) {
-    return `<div class="user-card" onclick="showUserPosts(${user.id})">
-    <div class="user-card__container">
-        <h3>${user.name}</h4>
-        <p><b>Email:</b> ${user.email}</p>
-        <p><b>Phone:</b> ${user.phone}</p>
-        <p><b>Website:</b> <a href="https://${user.name}" target="_blank">
-        ${user.website}
-        </a></p>
-    </div>
-</div>`
+    return `
+        <div class="user-card" onclick="showUserPosts(${user.id})">
+        <div class="user-card__container">
+            <h3>${user.name}</h3>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+        </div>
+        </div>
+    `
 }
